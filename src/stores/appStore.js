f7e32e91b4338533c8e96f9ec28764dfc0532860
BottyIvan <testbottyivan@gmail.com>
2025-06-19 22:47:03 +0200

    fix: Add a blank line for improved code readability in appStore.js

--- src/stores/appStore.js
+++ src/stores/appStore.js
@@ -2,6 +2,7 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useAppStore = defineStore('app', () => {
+
   const releases = ref([])
   const latestRelease = ref(null)
   const appInfo = ref({})
