69c45d188e92c1c76ec78b2844b99f5e1e5e6677
BottyIvan <testbottyivan@gmail.com>
2025-06-19 23:22:37 +0200

    fix: Update logo path in introfy.config.js for correct asset reference

--- introfy.config.js
+++ introfy.config.js
@@ -83,7 +83,7 @@ export default {
         }
     ],
     theme: {
-        logo: "./public/icon-512-maskable.png",
+        logo: "./icon-512-maskable.png",
         nav: [
             { title: "Features", link: "#features" },
             { title: "Main Features", link: "#main-features" },
