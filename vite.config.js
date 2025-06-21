6e2bdb733489e2d5bcd4ec9beb483d59453cbe04
BottyIvan <testbottyivan@gmail.com>
2025-06-21 07:25:45 +0200

    fix: Remove URL configuration and reset base path in Vite setup

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,7 @@ import vitePluginCopyMarkdownPages from './plugins/markdown-pages.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  url: 'https://bottyivan.github.io',
-  base: '/introfy/',
+  base: '/',
   plugins: [
     tailwindcss(),
     vue({
