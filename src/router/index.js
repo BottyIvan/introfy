36da7dfda5f6fd75d2aef17c457bd9a486567128
BottyIvan <testbottyivan@gmail.com>
2025-07-05 18:24:05 +0200

    fix: Change router history mode from createWebHistory to createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Home from '@/components/Home.vue'
 import Markdown from '@/components/Markdown.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 // Import all .md pages as Vue components
 const pages = import.meta.glob('../pages/**/*.md')
@@ -31,8 +31,7 @@ const routes = [
 ]
 
 const router = createRouter({
-
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes
 })
 
