1048f945a3f05e1d7f39e9023a1b266417e7b941
BottyIvan <testbottyivan@gmail.com>
2025-07-06 01:26:37 +0200

    fix: Update RouterLink to use subItem name for navigation consistency

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -102,7 +102,7 @@ defineProps({
                                             <ul
                                                 class="absolute left-0 mt-1 min-w-[140px] bg-white dark:bg-slate-900 rounded shadow border z-50 hidden group-hover:block">
                                                 <li v-for="subItem in item.pages" :key="subItem.name" class="py-1">
-                                                    <RouterLink v-if="subItem.path" :to="subItem.path"
+                                                    <RouterLink v-if="subItem.name" :to="subItem.name"
                                                         class="block px-3 py-1 hover:bg-gray-100 dark:hover:bg-gray-800 truncate">
                                                         {{ subItem.name }}
                                                     </RouterLink>
